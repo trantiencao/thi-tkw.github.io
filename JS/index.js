ac7ff08d072948889278b72234641093de128f93
Trần Tiến Cao <58876777+trantiencao@users.noreply.github.com>
2020-08-15 10:01:45 +0700

    thi-xong
    thi-xong

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,5 @@
 $(document).ready(function() {
-    // $('.login').hide();
-    // $('.signup').hide();
-    // $('#login').click(function(e) {
-    //     e.preventDefault();
-    //     $('.signup').hide();
-    //     $('.login').show();
-    // });
-    // $('#signup').click(function(e) {
-    //     e.preventDefault();
-    //     $('.login').hide();
-    //     $('.signup').show();
-    // });
-    // $('#logo').click(function(e) {
-    //     e.preventDefault();
-    //     $('.login').hide();
-    //     $('.signup').hide();
-    // });
+    $('#hello-User').hide();
 
     $('#actionlogin').click(function(e) {
         e.preventDefault();
@@ -23,23 +7,24 @@ $(document).ready(function() {
         let password = $('#passwordlogin').val();
         console.log(user);
         console.log(password);
-        if (user == 'admin' && password == 'admin') {
+        if (user == 'abc' && password == 'xyz') {
             alert("Đăng nhập thành công.");
             $('#login').hide();
             $('.form-login').hide(400);
+            $('#signup').hide();
             $('.modal-backdrop').remove('.modal-backdrop');
+            $('#body').removeClass('modal-open');
+            $('#hello-User').show();
         } else {
             alert("user hoặc password không đúng.");
         }
     });
 
-
-
     $('#actionsignup').click(function(e) {
         e.preventDefault();
         alert("Đăng ký thành công.");
-        $('#signup').hide();
         $('.form-signup').hide(400);
+        $('#signup').hide();
         $('.modal-backdrop').remove('.modal-backdrop');
     });
 });
